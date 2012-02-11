08391ffa380bb0ee2694a4e01b18abea0f067604
asus_user <asus_user@F3J-M.(none)>
2012-02-11 16:16:27 +0800

    Add test.c user program.

---
+++ test.c
@@ -0,0 +1,16 @@
+/*
+ * Filename: test.c
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+int main(void)
+{
+	int fd;
+	fd = open("/dev/cdata", O_RDWR);
+	close(fd);
+}
